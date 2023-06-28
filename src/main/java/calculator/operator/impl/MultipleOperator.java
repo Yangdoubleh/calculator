c4268e160dddfc5baf8b0de6c593d4667bce3f96
Yangdoubleh <why374@naver.com>
2023-06-28 15:51:51 +0900

    Feat(#6-operator-strategy) : 각각의 연산자 싱글톤 패턴으로 생성.

--- src/main/java/calculator/operator/impl/MultipleOperator.java
+++ src/main/java/calculator/operator/impl/MultipleOperator.java
@@ -5,6 +5,15 @@ import calculator.operator.Operator;
 import java.math.BigDecimal;
 
 public class MultipleOperator implements Operator {
+    private static MultipleOperator multipleOperator = new MultipleOperator();
+
+    private MultipleOperator() {
+    }
+
+    public static MultipleOperator getInstance() {
+        return multipleOperator;
+    }
+
     @Override
     public BigDecimal operator(BigDecimal num1, BigDecimal num2) {
         return num1.multiply(num2);
--- src/main/java/calculator/operator/impl/PlusOperator.java
+++ src/main/java/calculator/operator/impl/PlusOperator.java
@@ -5,6 +5,15 @@ import calculator.operator.Operator;
 import java.math.BigDecimal;
 
 public class PlusOperator implements Operator {
+    private static PlusOperator plusOperator = new PlusOperator();
+
+    private PlusOperator() {
+    }
+
+    public static PlusOperator getInstance() {
+        return plusOperator;
+    }
+
     @Override
     public BigDecimal operator(BigDecimal num1, BigDecimal num2) {
         return num1.add(num2);
