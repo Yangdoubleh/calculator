7b6796096eace5fe737775f50e528927b494ac4c
Yangdoubleh <why374@naver.com>
2023-06-29 17:17:54 +0900

    Feat(#8-operator-factory) : 06-29 작업내용 일부 커밋.

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,16 @@
 package calculator;
 
+import calculator.factory.OperatorFactory;
+import calculator.operator.Operator;
 import calculator.operator.impl.PlusOperator;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
 
 public class Calculator {
 
     private static Calculator calculator = new Calculator();
+    private final OperatorFactory operatorFactory = OperatorFactory.getInstance();
 
     private Calculator() {
     }
@@ -13,9 +19,13 @@ public class Calculator {
         return calculator;
     }
 
-    public void calResult(String cal) {
+    public BigDecimal calResult(String cal) {
         try {
+            for (byte b : cal.getBytes()) {
 
+            }
+            Operator operator = operatorFactory.createOperator("");
+            return operator.operator();
         } catch (RuntimeException e) {
             System.out.println(e);
         }
--- src/main/java/calculator/factory/OperatorFactory.java
+++ src/main/java/calculator/factory/OperatorFactory.java
@@ -2,11 +2,39 @@ package calculator.factory;
 
 import calculator.factory.constant.OperatorType;
 import calculator.operator.Operator;
+import calculator.operator.impl.DevideOperator;
+import calculator.operator.impl.MinusOperator;
+import calculator.operator.impl.MultipleOperator;
+import calculator.operator.impl.PlusOperator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static calculator.factory.constant.OperatorType.*;
 
 public class OperatorFactory {
 
+    private static OperatorFactory operatorFactory = new OperatorFactory();
+    private OperatorFactory() {
+        typeOperatorMap.put(PLUS, PlusOperator.getInstance());
+        typeOperatorMap.put(MINUS, MinusOperator.getInstance());
+        typeOperatorMap.put(MULTIPLE, MultipleOperator.getInstance());
+        typeOperatorMap.put(DEVIDE, DevideOperator.getInstance());
+    }
+
+    public static OperatorFactory getInstance() {
+        return operatorFactory;
+    }
+
+    private static final Map<OperatorType, Operator> typeOperatorMap = new HashMap<>();
+
+
     public Operator createOperator(String operator) {
-        OperatorType operatorType = OperatorType.getOperator(operator);
-        
+        try {
+            OperatorType operatorType = OperatorType.getOperator(operator);
+            return typeOperatorMap.get(operatorType);
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException("유효하지 않은 계산 기호 입니다.");
+        }
     }
 }
